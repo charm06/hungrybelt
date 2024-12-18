75432710637aff90ae9924be673e81ad8b5789f7
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-12-19 01:18:24 +0800

    feat(home_page): added search feature

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:hungrybelt/pages/filter_page.dart';
 import 'package:hungrybelt/app_state.dart';
 import 'package:hungrybelt/components/app_drawer.dart'; // Import the AppDrawer
 import 'package:hungrybelt/components/food_place_carousel.dart';
+import 'package:hungrybelt/pages/search_page.dart';
 import 'package:provider/provider.dart';
 import 'package:get/get.dart';
 
@@ -14,12 +15,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var selectedFoodPlace = null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: SizedBox(
-          height: 200.0, 
+          height: 200.0,
           child: Image.asset('assets/images/uHome_title.png'),
         ),
       ),
@@ -27,135 +30,180 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Top Recommendations Section
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10.0),
-                child: Text(
-                  "Top Recommendations",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15.0,
+          child: Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Top Recommendations Section
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Text(
+                    "Top Recommendations",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 25),
-
-              // Placeholder for Top Recommendations Content
-              // Food Place Carousel Section
-              Consumer<ApplicationState>(
-                builder: (context, appState, _) => FoodPlaceCarousel(
-                  foodPlaces: appState.foodPlace,
+                const SizedBox(
+                  height: 10,
                 ),
-              ),
-
-              // Filter Section
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10.0),
-                child: Text(
-                  "UFilter",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15.0,
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Navigate to SearchPage when pressed
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SearchPage(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      side: const BorderSide(color: Colors.grey),
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      backgroundColor: Colors.white,
+                    ),
+                    child: const Row(
+                      children: [
+                        Icon(
+                          Icons.search,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: Text(
+                            "Search for food places",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 16,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 25),
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Cafe filter
-                  _buildFilterOption(
-                    context,
-                    'assets/images/filter_cafe.png',
-                    'Cafe',
-                    const FilterPage(filterCategory: 'Cafe'),
+                const SizedBox(height: 25),
+
+                // Placeholder for Top Recommendations Content
+                // Food Place Carousel Section
+                FoodPlaceCarousel(
+                  foodPlaces: appState.foodPlaces,
+                ),
+
+                // Filter Section
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Text(
+                    "UFilter",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                    ),
                   ),
-                  const SizedBox(width: 25),
+                ),
+                const SizedBox(height: 25),
 
-                  // Drinks filter
-                  _buildFilterOption(
-                    context,
-                    'assets/images/filter_drinks.png',
-                    'Drinks',
-                    const FilterPage(filterCategory: 'Drinks'),
-                  ),
-                  const SizedBox(width: 25),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Cafe filter
+                    _buildFilterOption(
+                      context,
+                      'assets/images/filter_cafe.png',
+                      'Cafe',
+                      const FilterPage(filterCategory: 'Cafe'),
+                    ),
+                    const SizedBox(width: 25),
 
-                  // Meal filter
-                  _buildFilterOption(
-                    context,
-                    'assets/images/filter_meal.png',
-                    'Meal',
-                    const FilterPage(filterCategory: 'Meal'),
-                  ),
-                  const SizedBox(width: 25),
+                    // Drinks filter
+                    _buildFilterOption(
+                      context,
+                      'assets/images/filter_drinks.png',
+                      'Drinks',
+                      const FilterPage(filterCategory: 'Drinks'),
+                    ),
+                    const SizedBox(width: 25),
 
-                  // Snack filter
-                  _buildFilterOption(
-                    context,
-                    'assets/images/filter_snack.png',
-                    'Snack',
-                    const FilterPage(filterCategory: 'Snack'),
-                  ),
+                    // Meal filter
+                    _buildFilterOption(
+                      context,
+                      'assets/images/filter_meal.png',
+                      'Meal',
+                      const FilterPage(filterCategory: 'Meal'),
+                    ),
+                    const SizedBox(width: 25),
 
-                ],
-              ),
-              const SizedBox(height: 25),
-            ],
+                    // Snack filter
+                    _buildFilterOption(
+                      context,
+                      'assets/images/filter_snack.png',
+                      'Snack',
+                      const FilterPage(filterCategory: 'Snack'),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 25),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-
-Widget _buildFilterOption(BuildContext context, String imagePath, String label, Widget destinationPage) {
-  return Column(
-    children: [
-      GestureDetector(
-        onTap: () {
-          // Use Get.to for navigation
-          Get.to(() => destinationPage);
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.3),
-                blurRadius: 6.0,
-                offset: const Offset(2, 2),
+  Widget _buildFilterOption(BuildContext context, String imagePath,
+      String label, Widget destinationPage) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () {
+            // Use Get.to for navigation
+            Get.to(() => destinationPage);
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  blurRadius: 6.0,
+                  offset: const Offset(2, 2),
+                ),
+              ],
+              border: Border.all(
+                color: const Color(0xFFD6D5D5),
+                width: 2.0,
               ),
-            ],
-            border: Border.all(
-              color: const Color(0xFFD6D5D5),
-              width: 2.0,
+              borderRadius: BorderRadius.circular(15),
             ),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(13),
-            child: Image.asset(
-              imagePath,
-              height: 65,
-              width: 65,
-              fit: BoxFit.cover,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(13),
+              child: Image.asset(
+                imagePath,
+                height: 65,
+                width: 65,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
         ),
-      ),
-      const SizedBox(height: 1),
-      Text(
-        label,
-        style: const TextStyle(
-          fontSize: 11.0,
-          fontWeight: FontWeight.bold,
+        const SizedBox(height: 1),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 11.0,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-      ),
-    ],
-  );
-}
+      ],
+    );
+  }
 }
---
+++ lib/pages/search_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:hungrybelt/models/food_place.dart';
+import 'package:hungrybelt/app_state.dart';
+import 'package:hungrybelt/pages/info_page.dart';
+import 'package:provider/provider.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  late TextEditingController _controller;
+  List<FoodPlace> _filteredFoodPlaces = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _filteredFoodPlaces = context.read<ApplicationState>().foodPlaces;
+    _controller.addListener(_onSearchChanged);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // Update the filtered list based on the search text
+  void _onSearchChanged() {
+    final query = _controller.text.toLowerCase();
+    setState(() {
+      _filteredFoodPlaces = context
+          .read<ApplicationState>()
+          .foodPlaces
+          .where((foodPlace) => foodPlace.name
+              .toLowerCase()
+              .contains(query)) // Filter based on name
+          .toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: _controller,
+          decoration: InputDecoration(
+            hintText: 'Search for food places...',
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(24),
+              borderSide: const BorderSide(
+                width: 1,
+                color: Colors.grey,
+                style: BorderStyle.solid,
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(24),
+              borderSide: const BorderSide(
+                width: 1,
+                color: Colors.black,
+                style: BorderStyle.solid,
+              ),
+            ),
+            fillColor: Colors.white,
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+          ),
+        ),
+      ),
+      body: _controller.text.isEmpty
+          ? Container() // Empty container when search field is empty
+          : ListView(
+              children: [
+                // Display suggestions as a list when typing
+                for (var foodPlace in _filteredFoodPlaces)
+                  ListTile(
+                    title: Text(foodPlace.name),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => InfoPage(
+                            foodPlace: foodPlace,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+              ],
+            ),
+    );
+  }
+}
